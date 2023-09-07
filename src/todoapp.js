716da09197d2c63c691502a82a37f29b0c2087f8
agusRobaina <ar269622@fi365.ort.edu.uy>
2023-09-06 23:14:24 -0300

    add : Implementación de uncompleteTask

--- src/todoapp.js
+++ src/todoapp.js
@@ -23,7 +23,8 @@ const completeTask = (name) => {
 }
 
 const uncompleteTask = (name) => {
-    //IMPLEMENTAR
+    const task = tasks.find(task => task.name === name);
+    task.done = true;
 }
 
 const addTimer = (name, time) => {
